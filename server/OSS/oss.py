6045714998cb28486850776f5e5d9cfd7a80355d
roweax <roweax@gmail.com>
2017-07-16 12:01:51 +0800

    update oss

--- server/OSS/oss.py
+++ server/OSS/oss.py
@@ -5,10 +5,23 @@ class AliyunOSS:
     def __init__(self):
         self.auth = oss2.Auth(Config.oss_access_key_id, Config.oss_access_key_secret)
         self.service = oss2.Service(self.auth, Config.oss_backet)
-
         #print([b.name for b in oss2.BucketIterator(service)])
+
+
     def Save(self, source, target):
+        success = False
+        while not success:
+            try :
+                self.CoreSave(source, target)
+            except Exception, e:
+                print(e)
+            else :
+                success = True
+
+
+    def CoreSave(self, source, target) :
         bucket = oss2.Bucket(self.auth, Config.oss_backet, 'suidev')
         bucket.put_object_from_file(target, source)
         print("Saved OSS file %s"%target)
+
         
